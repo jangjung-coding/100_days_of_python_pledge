88b521a925089a23acb2b4a6140c66521a185913
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-09-04 13:17:56 +0900

    Day1 complete

---
+++ Day1/Making BandName.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+# 이름 물어보기
+name = input("What is your name?\n")
+# 반려동물 이름 물어보기
+petName = input("What is your pet's name?\n")
+# 둘을 합쳐서 밴드 이름 만들기
+print("\nYour band name could be: " + name + " " + petName)
---
+++ Day1/task.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("Hello, World!\nHello, World!")
+print("Hello" + " " + "Jang")
+
+#python input
+name = input("What is your name?")
+print("Hello, " + name)
+
+name2 = input("What is your namem, again?")
+print("Length of your name is: ")
+print(len(name2))
+
