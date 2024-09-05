9324fb5cf3f7b7fd618603dfb5a8d79bed243f24
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-09-05 21:53:46 +0900

    Day3 Update

---
+++ Day3/class.py
@@ -0,0 +1,13 @@
+# Comparison Operators
+# ==, !=, >, <, >=, <=
+
+# Modulo Operator
+# % - returns the remainder of the division of the number to the left by the number on its right
+# print(10 % 3) # 1
+
+# Check Odd or Even
+# number = int(input("Enter a number:"))
+# if number  % 2 == 0:
+#   print(f"{number} is even number")
+# else:
+#   print(f"{number} is odd number")
