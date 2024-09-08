67bee67ae46786f70816063d7d93fd9d529a898e
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-09-08 19:35:31 +0900

    Day5 Complete

---
+++ Day5/class.py
@@ -0,0 +1,21 @@
+# fruits = ["Apple", "Banana", "Pear"]
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + " Pie")
+
+# student_scores = [90, 80, 20, 30, 10, 50]
+
+
+# max_score = 0
+# for score in student_scores:
+#   if score > max_score:
+#     max_score = score
+
+# print(f"The highest score in the class is: {max_score}")
+
+
+gauses_number = 0
+for number in range(1,101):
+  gauses_number += number
+  
+print(gauses_number)
