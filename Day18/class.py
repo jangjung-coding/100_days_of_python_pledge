82f61c1483648359d1a184b38a05f9b8ee225004
jangjung-coding <jangisalive1650@gmail.com>
2024-09-21 21:24:30 +0900

    Day18 Complete

--- Day18/class.py
+++ Day18/class.py
@@ -18,14 +18,28 @@ timmy_the_turtle.color("green")
 # Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon
 turtle_colors = ["red", "green", "blue", "yellow", "purple", "orange", "black", "pink", "brown", "cyan"]
 
-for number_of_sides in range(3, 11):
-    angle = 360 / number_of_sides
-    random_color = random.choice(turtle_colors)
-    timmy_the_turtle.color(random_color)
+# for number_of_sides in range(3, 11):
+#     angle = 360 / number_of_sides
+#     random_color = random.choice(turtle_colors)
+#     timmy_the_turtle.color(random_color)
     
-    for _ in range(number_of_sides):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(angle)
+#     for _ in range(number_of_sides):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(angle)
+
+# Draw a random walk
+# directions = [0, 90, 180, 270]
+
+# for _ in range(200):
+#     timmy_the_turtle.forward(random.randint(10, 50))
+#     timmy_the_turtle.setheading(random.choice(directions))
+
+# Draw a spirograph
+timmy_the_turtle.speed("fast")
+for _ in range(72):
+    timmy_the_turtle.color(random.choice(turtle_colors))
+    timmy_the_turtle.circle(100)
+    timmy_the_turtle.setheading(timmy_the_turtle.heading() + 5)
 
 screen = Screen()
 screen.exitonclick()
---
+++ Day18/hirst-painting-start/main.py
@@ -0,0 +1,49 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('Day18\hirst-painting-start\image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+# Draw the dots
+import turtle
+import random
+
+turtle.colormode(255)
+tim = turtle.Turtle()
+tim.shape("turtle")
+color_list = rgb_colors
+tim.speed("fastest")
+
+tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(350)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.penup()
+    tim.forward(50)
+    
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle.Screen()
+screen.exitonclick()
+
+
+
+
+
