b6aa2fd4ec74abb23955d1ef0cfe0e63e6941186
jangjung-coding <jangisalive1650@gmail.com>
2024-09-21 20:03:12 +0900

    Day18 Update - Draw a dashed line

--- Day18/class.py
+++ Day18/class.py
@@ -4,11 +4,15 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
 
-for _ in range(4): # Make a square
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# for _ in range(4): # Make a square
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
 
-
+for _ in range(15): # Draw a dashed line
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
 
 
 screen = Screen()
