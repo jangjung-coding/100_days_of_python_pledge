f9283fcde575acc305f0a1d192b734f62654660c
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-09-23 17:48:36 +0900

    Day20 Complete

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -1,21 +1,27 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-# 1. Create a snake body
-segment_1 = Turtle("square")
-segment_1.color("white")
+snake = Snake()
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(x=-20, y=0)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(x=-40, y=0)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 
 screen.exitonclick()
