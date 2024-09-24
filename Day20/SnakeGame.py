e8e5ccdde8abfe99d1a83802ea7afcca8fd26c31
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-09-24 22:20:57 +0800

    Day21 Complete

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreBoard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreBoard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +24,17 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
-
-
+    
+    # Detect collision with food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreBoard.increase_score()
+    
+    # Detect collision with wall
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
+        game_is_on = False
+        scoreBoard.game_over()
+    
 screen.exitonclick()
