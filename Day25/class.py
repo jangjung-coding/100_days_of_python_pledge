1ff84bdf15b8bdb8c6a0f6d2ec20a9e1223a78df
jangjung-coding <jangisalive1650@gmail.com>
2024-10-01 17:45:32 +0900

    Day25 Update

--- Day25/class.py
+++ Day25/class.py
@@ -2,7 +2,12 @@
 #     data = file.readlines()
 #     print(data[:5])
 
+# import pandas as pd
+
+# weather = pd.read_csv('weather_data.csv')
+# print(weather.head())
+
 import pandas as pd
 
-weather = pd.read_csv('weather_data.csv')
-print(weather.head())
+data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+print(data['Primary Fur Color'].value_counts())
