0b94b40d5f4a3e722bb09cd977eaa901ba18e385
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-16 13:44:17 +0900

    Day33 Create

---
+++ Day33/class.py
@@ -0,0 +1,8 @@
+# What is API?
+'''
+An Application Programming Interface (API) is 
+a set of rules and protocols that allows one software application to interact with another. 
+In other words, an API is the messenger that delivers your request to the provider 
+that you're requesting it from and then delivers the response back to you.
+'''
+
