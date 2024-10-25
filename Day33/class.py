34b3d1b1f73ac769a02b6cacc4b71e9f986c5bdc
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-25 15:28:36 +0900

    Day33 Update

--- Day33/class.py
+++ Day33/class.py
@@ -6,3 +6,24 @@ In other words, an API is the messenger that delivers your request to the provid
 that you're requesting it from and then delivers the response back to you.
 '''
 
+# What is API endpoint?
+'''
+An API endpoint is a point at which an application program interface (API)
+sends or receives information from a server.
+'''
+
+import requests
+
+response = requests.get(url='http://api.open-notify.org/iss-now.json')
+print(response) # <Response [200]>
+print(response.status_code) # 200
+
+# Response Codes
+'''
+1XX: Hold on
+2XX: Here you go
+3XX: Go away
+4XX: You screwed up
+5XX: I screwed up
+'''
+
