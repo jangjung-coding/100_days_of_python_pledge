082bc2471fb5fb872f46ecd8c3cc5e3f9aee15ef
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-28 13:41:21 +0900

    Day34 Update

---
+++ Day34/quizzler-app-start/class.py
@@ -0,0 +1,9 @@
+# Type Hints
+# 1. Type hints are a way to specify the type of a variable in Python.
+# 2. Type hints are not enforced by Python, but they are useful for developers to understand the intended usage of a function or method.
+# 3. Type hints can be used to specify the type of arguments and return values of functions and methods.
+# 4. Type hints can be added using the `:` syntax after the variable name or function parameter name.
+# 5. Type hints can be added to variables, function parameters, and return values.
+
+def add(a: int, b: int) -> int:
+    return a + b
