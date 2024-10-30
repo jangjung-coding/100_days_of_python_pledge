01650ee4da92cc564e4227c94a1502d47fb8a380
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-30 16:46:30 +0900

    Day35 Update

--- Day35/main.py
+++ Day35/main.py
@@ -1,11 +1,23 @@
-
+api_key = "1dcb8481a1afe10a993730da0629a9fb"
 
 # 1. Get your latitude and longitude from latlong.net
-latitude = "35.162441"
-longtitude = "126.910339"
+latitude = "33.44"
+longtitude = "-94.04"
 
 # 2. Make a request to the One Call API using the requests module
+import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+
+weather_params = {
+    "lat": latitude,
+    "lon": longtitude,
+    "appid": api_key,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code) # This takes some time to get the response
+print(response.json())
 
 # 3. Print out the HTTP status code that you get back
 
