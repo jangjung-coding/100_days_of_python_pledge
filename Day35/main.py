afd1750523c4d8aff38a7c7023a5c47090d1cbb5
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-30 17:12:45 +0900

    Day35 Update

--- Day35/main.py
+++ Day35/main.py
@@ -1,28 +1,26 @@
 api_key = "1dcb8481a1afe10a993730da0629a9fb"
 
 # 1. Get your latitude and longitude from latlong.net
-latitude = "33.44"
-longtitude = "-94.04"
+latitude = "35.162441"
+longtitude = "126.910339"
 
 # 2. Make a request to the One Call API using the requests module
 import requests
 
-OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
 
 weather_params = {
     "lat": latitude,
     "lon": longtitude,
     "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
-response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code) # This takes some time to get the response
-print(response.json())
-
 # 3. Print out the HTTP status code that you get back
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status() # This takes some time to get the response
+weather_data = response.json()
 
 # 4. Print the response to the console
-
-# 5. Copy-paste the response to an online JSON viewer(e.g. jsonviewer.stack.hu)
-
-# 6. Locate the hourly forecast for the next 48 hours
+print(weather_data["weather"][0]["description"]) # We can get the weather description
+print(weather_data["weather"][0]["id"]) # We can get the weather id
