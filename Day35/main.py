960fc775f69886e0fd42c58fad575590f6cced05
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-10-30 15:42:56 +0900

    Day35 Create

---
+++ Day35/main.py
@@ -0,0 +1,16 @@
+
+
+# 1. Get your latitude and longitude from latlong.net
+latitude = "35.162441"
+longtitude = "126.910339"
+
+# 2. Make a request to the One Call API using the requests module
+
+
+# 3. Print out the HTTP status code that you get back
+
+# 4. Print the response to the console
+
+# 5. Copy-paste the response to an online JSON viewer(e.g. jsonviewer.stack.hu)
+
+# 6. Locate the hourly forecast for the next 48 hours
