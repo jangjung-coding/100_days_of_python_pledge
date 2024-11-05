f148876e11dc68fe7300cf80fd1a3842acd64672
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-11-05 17:05:25 +0900

    Day37 Update

--- Day37/class.py
+++ Day37/class.py
@@ -6,3 +6,33 @@ PUT: Update data on the server
 DELETE: Delete data from the server
 '''
 
+# Pixela API
+import requests
+
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers={"X-USER-TOKEN": TOKEN})
+# print(response.text)
+
