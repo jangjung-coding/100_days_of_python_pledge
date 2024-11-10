20dfda8491ec99fdef14bf877f856b2a806a1d48
JANG JUNGWOO <jangisalive1650@gmail.com>
2024-11-10 16:36:22 +0900

    Day37 Complete

--- Day37/class.py
+++ Day37/class.py
@@ -9,7 +9,9 @@ DELETE: Delete data from the server
 # Pixela API
 import requests
 
-
+TOKEN = "kictIntern-20241105"
+USERNAME = "jjang"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -26,7 +28,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "Km",
     "type": "float",
@@ -35,4 +37,17 @@ graph_config = {
 
 # response = requests.post(url=graph_endpoint, json=graph_config, headers={"X-USER-TOKEN": TOKEN})
 # print(response.text)
+
+import datetime
+today = datetime.datetime.now()
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "7",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers={"X-USER-TOKEN": TOKEN})
+print(response.text)
+
 
